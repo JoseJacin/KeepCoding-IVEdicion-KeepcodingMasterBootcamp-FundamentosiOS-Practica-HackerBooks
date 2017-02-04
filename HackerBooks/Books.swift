cc9906557f84486710851b4daf0bdb0024bcac17
José Sánchez <joseynene@hotmail.com>
2017-02-04 11:10:54 +0100

    Generada la clase Book. Contiene los proxies, las extensiones y los typealias

---
+++ HackerBooks/Books.swift
@@ -0,0 +1,9 @@
+//
+//  Books.swift
+//  HackerBooks
+//
+//  Created by Jose Sanchez Rodriguez on 4/2/17.
+//  Copyright © 2017 Jose Sanchez Rodriguez. All rights reserved.
+//
+
+import Foundation
